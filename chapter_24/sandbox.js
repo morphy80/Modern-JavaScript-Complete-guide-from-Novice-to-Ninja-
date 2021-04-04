6bc448612709231982a199237c93bb7b5422a489
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-04 22:40:38 +0200

    Function Declaration & Expression - Lecture 30

---
+++ chapter_24/sandbox.js
@@ -0,0 +1,14 @@
+// function declaration
+function greet() {
+    console.log('hello there');
+}
+
+greet();
+
+// function expression
+const speak = function() {
+    console.log('good day!');
+};
+
+speak();
+speak();
