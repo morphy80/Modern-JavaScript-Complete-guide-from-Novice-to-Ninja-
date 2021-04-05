8ebca5faaf8ebb478098897119e262fe6cc9edbd
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-05 14:25:25 +0200

    Return Values - Lecture 32

---
+++ chapter_26/sandbox.js
@@ -0,0 +1,16 @@
+// return statements
+
+const calcArea = function(radius) {
+    // let area = 3.14 * radius ** 2;
+    // return area;
+    return 3.14 * radius ** 2;
+};
+
+const area = calcArea(5);
+console.log('area is:', area);
+
+const calcVol = function(area) {
+
+};
+
+calcVol(area);
