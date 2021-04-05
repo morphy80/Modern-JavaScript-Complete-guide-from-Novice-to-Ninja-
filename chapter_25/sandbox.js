cffaf2c4ec0557478fc16281335aaea5ecfd4527
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-05 14:04:17 +0200

    Arguments & Parameters - Lecture 31

---
+++ chapter_25/sandbox.js
@@ -0,0 +1,10 @@
+// arguments & parameters
+
+const speak = function(name = 'luigi', time = 'night') {
+    console.log(`good ${time} ${name}`);
+};
+
+// speak('mario', 'morning');
+speak();
+speak('shaun', 'day');
+speak('mario');
