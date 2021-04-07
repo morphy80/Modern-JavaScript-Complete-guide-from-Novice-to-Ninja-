0907078c84b21d329574955f2d36e85146b31f1f
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-08 00:03:27 +0200

    "this" Keyword - Lecture 40

---
+++ section_5/chapter_33/sandbox.js
@@ -0,0 +1,26 @@
+// object literals
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac&cheese rules', '10 things to make with marmite'],
+    login() {
+        console.log('the user logged in');
+    },
+    logout() {
+        console.log('the user logged out');
+    },
+    logBlogs() {
+        // access the blogs here
+        // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        })
+    }
+};
+
+user.logBlogs();
+console.log(this);
