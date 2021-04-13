be4d81f49a761c06211308b273a40f4978029a65
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-13 12:23:32 +0200

    The Query Selector - Lecture 46

---
+++ section_6/chapter_37/sandbox.js
@@ -0,0 +1,14 @@
+// const para = document.querySelector('p');
+// const para = document.querySelector('.error');
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+// query multiple elements at once
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+// paras.forEach(para => {
+//     console.log(para);
+// });
+console.log(errors);
+// console.log(paras[1]);
