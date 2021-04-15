ad622793904db8305265ead477bad9ad57ef7612
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-15 13:30:08 +0200

    Creating & Removing Elements - Lecture 54

---
+++ section_6/chapter_45/sandbox.js
@@ -0,0 +1,22 @@
+const ul = document.querySelector('ul');
+// ul.remove();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    // ul.innerHTML += '<li>something new</li>';
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    // ul.append(li);
+    ul.prepend(li);
+});
+
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+    item.addEventListener('click', event => {
+
+        // event.target.style.textDecoration = 'line-through';
+        event.target.remove();
+    });
+});
