be68858a3e4c97ebe6cf08479ac5404b4461bb65
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-23 23:32:05 +0200

    Intervals & Animating Score - Lecture 70

--- section_8_quiz_project/app.js
+++ section_8_quiz_project/app.js
@@ -17,20 +17,29 @@ form.addEventListener('submit', e => {
 
     // show the result
     scrollTo(0, 0);
-    result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+
 });
-
-//  window object (global object)
-// console.log('hello');
-// window.console.log('hello');
-
-// console.log(document.querySelector('form'));
-// console.log(window.document.querySelector('form'));
-
-// alert('hello');
-// window.alert('hello');
 
 // setTimeout(() => {
 //     alert('hello ninjas');
 // }, 3000);
+
+// let i = 0;
+// const timer = setInterval(() => {
+//     console.log('hello');
+//     i++;
+//     if (i === 5) {
+//         clearInterval(timer);
+//     }
+// }, 1000);
