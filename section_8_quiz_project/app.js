07cc918645a7167f11aa1d74e50d706057204571
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-23 22:20:00 +0200

    The window object - Lecture 69

--- section_8_quiz_project/app.js
+++ section_8_quiz_project/app.js
@@ -16,6 +16,21 @@ form.addEventListener('submit', e => {
     });
 
     // show the result
+    scrollTo(0, 0);
     result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
 });
+
+//  window object (global object)
+// console.log('hello');
+// window.console.log('hello');
+
+// console.log(document.querySelector('form'));
+// console.log(window.document.querySelector('form'));
+
+// alert('hello');
+// window.alert('hello');
+
+// setTimeout(() => {
+//     alert('hello ninjas');
+// }, 3000);
