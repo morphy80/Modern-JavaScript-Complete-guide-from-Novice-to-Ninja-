8865e54e93124ad9f664430d1ee676648cebdb4f
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-24 21:35:23 +0200

    Deleting Todos - Lecture 80

--- section_10_todos_project/app.js
+++ section_10_todos_project/app.js
@@ -12,6 +12,7 @@ const generateTemplate = (todo) => {
     list.innerHTML += html;
 };
 
+// add todos
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
@@ -20,4 +21,13 @@ addForm.addEventListener('submit', (e) => {
         generateTemplate(todo);
         addForm.reset();
     }
+});
+
+// delete todos
+list.addEventListener('click', e => {
+
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+
 });
