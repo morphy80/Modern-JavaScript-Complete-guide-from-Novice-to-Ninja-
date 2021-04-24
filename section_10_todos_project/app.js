9918dfe009338dd05c858ca1340c80f6c163a317
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-24 22:02:47 +0200

    Searching & Filtering Todos - Lecture 81

--- section_10_todos_project/app.js
+++ section_10_todos_project/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = (todo) => {
     const html = `
@@ -30,4 +31,23 @@ list.addEventListener('click', e => {
         e.target.parentElement.remove();
     }
 
+});
+
+const filterTodos = (term) => {
+
+    // add filtered class
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.add('filtered'));
+
+    // remove filtered class
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove('filtered'));
+};
+
+// keyup event 
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
