027705af4cfb677bbd9a5ff04590fa33784b0a4d
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-25 23:35:14 +0200

    Promise Basics - Lecture 94

---
+++ section_12/lecture_94/sandbox.js
@@ -0,0 +1,50 @@
+const getTodos = (resource) => {
+
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', () => {
+
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting resource');
+            }
+
+        });
+
+        request.open('GET', resource);
+        request.send();
+    });
+
+};
+
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise resolved:', data);
+}).catch(err => {
+    console.log('promise rejected:', err);
+});
+
+// promise example
+
+// const getSomething = () => {
+
+//     return new Promise((resolve, reject) => {
+//         // fetch something 
+//         // resolve('some data');
+//         reject('some error');
+//     });
+// };
+
+// getSomething().then(data => {
+//   console.log('promise resolved:', data);
+// }, err => {
+//   console.log('promise rejected:', err);
+// });
+
+// getSomething().then(data => {
+//     console.log('promise resolved:', data);
+// }).catch(err => {
+//     console.log('promise rejected:', err);
+// });
