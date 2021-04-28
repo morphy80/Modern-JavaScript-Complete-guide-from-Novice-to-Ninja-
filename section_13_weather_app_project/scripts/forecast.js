07852ce084b181249b8119e44b6701c54f49c74b
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-04-28 15:19:58 +0200

    Get City API Call - Lecture 102

--- section_13_weather_app_project/scripts/forecast.js
+++ section_13_weather_app_project/scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = 'EovbZmDyF7umtSXq7MO6PvxEdmfMCH58';
+
+const getCity = async(city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+getCity('manchester')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
