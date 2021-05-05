0b1d4a8c208c6b3078a7942e8672858029aaff90
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-05-05 15:53:24 +0200

    Prototypal Inheritance - Lecture 123

---
+++ section_15/lecture_123/sandbox.js
@@ -0,0 +1,22 @@
+// constructor function
+
+function User(username, email) {
+    this.username = username;
+    this.email = email;
+}
+
+User.prototype.login = function() {
+    console.log(`${this.username} has logged in`);
+    return this;
+};
+
+User.prototype.logout = function() {
+    console.log(`${this.username} has logged out`);
+    return this;
+};
+
+const userOne = new User('ryu', 'ryu@thenetninja.co.uk');
+const userTwo = new User('chun-li', 'chun.li@thenetninja.co.uk');
+
+console.log(userOne, userTwo);
+userOne.login().logout();
