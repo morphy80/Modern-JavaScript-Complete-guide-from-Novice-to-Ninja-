9633b9ccd855142a8aaf718eea32663dd20a7d5f
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-05-05 22:33:46 +0200

    Deleting Documents - Lecture 132

--- section_16/sandbox.js
+++ section_16/sandbox.js
@@ -1,25 +1,55 @@
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
     let time = recipe.created_at.toDate();
     let html = `
-        <li>
-        <div>${recipe.title}</div>
-        <div><small>${time}</small></div>
+        <li data-id="${id}">
+            <div>${recipe.title}</div>
+            <div><small>${time}</small></div>
+            <button class="btn btn-danger btn-sm my-2">delete</button>
         </li>
         `;
 
     // console.log(html);
     list.innerHTML += html;
-}
+};
 
+// get documents
 db.collection('recipes').get().then(snapshot => {
-    // when we have the data
-    // console.log(snapshot.docs[0].data());
     snapshot.docs.forEach(doc => {
-        // console.log(doc.data());
-        addRecipe(doc.data());
+        // console.log(doc.id);
+        addRecipe(doc.data(), doc.id);
     });
 }).catch(err => {
     console.log(err);
+});
+
+// add documents 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+
+    db.collection('recipes').add(recipe).then(() => {
+        console.log('recipe added');
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+// deleting data
+list.addEventListener('click', e => {
+    // console.log(e);
+    if (e.target.tagName === 'BUTTON') {
+        const id = e.target.parentElement.getAttribute('data-id');
+        // console.log(id);
+        db.collection('recipes').doc(id).delete().then(() => {
+            console.log('recipe deleted');
+        });
+    }
 });
