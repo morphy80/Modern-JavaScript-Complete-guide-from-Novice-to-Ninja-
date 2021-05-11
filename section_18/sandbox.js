284b9881c23f81ead2e89c9a28386fe7c2875594
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-05-11 23:48:14 +0200

    Symbols - Lecture 150

--- section_18/sandbox.js
+++ section_18/sandbox.js
@@ -50,3 +50,24 @@ const ninjas = new Set([
 ninjas.forEach(ninja => {
     console.log(ninja.name, ninja.age);
 });
+
+
+// symbols
+
+const symbolOne = Symbol('a generic name');
+const symbolTwo = Symbol('a generic name');
+
+console.log(symbolOne, typeof symbolOne);
+console.log(symbolOne === symbolTwo);
+
+const ninja = {};
+
+ninja.age = 30;
+ninja['belt'] = 'orange';
+ninja['belt'] = 'black';
+
+ninja[symbolOne] = 'ryu';
+ninja[symbolTwo] = 'shaun';
+
+console.log(ninja);
+console.log(ninja[symbolOne], ninja[symbolTwo]);
