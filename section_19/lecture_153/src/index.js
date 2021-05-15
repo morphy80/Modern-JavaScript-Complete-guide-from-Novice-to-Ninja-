3dc58085edc16b4a8ce26023ab84fcaadd95fe72
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-05-16 00:42:12 +0200

    Watching for Changes - Lecture 161

--- section_19/lecture_153/src/index.js
+++ section_19/lecture_153/src/index.js
@@ -4,9 +4,4 @@ import users, { getPremUsers } from './data';
 const premUsers = getPremUsers(users);
 console.log(users, premUsers);
 
-console.log('index file');
-
-addTitle('test');
-styleBody();
-
-console.log(contact);
+console.log('test');
