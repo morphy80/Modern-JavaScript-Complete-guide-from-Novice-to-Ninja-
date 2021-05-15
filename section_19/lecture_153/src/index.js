5f358d248113362c7701167f03b1d97b2b098c0d
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-05-16 00:35:17 +0200

    Default Exports - Lecture 160

--- section_19/lecture_153/src/index.js
+++ section_19/lecture_153/src/index.js
@@ -1,4 +1,8 @@
 import { styleBody, addTitle, contact } from './dom';
+import users, { getPremUsers } from './data';
+
+const premUsers = getPremUsers(users);
+console.log(users, premUsers);
 
 console.log('index file');
 
