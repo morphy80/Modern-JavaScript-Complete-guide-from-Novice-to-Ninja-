0bc5666ec0aa96649960ed3e7df0735ceca0d634
Alexandru Apostoiu <56297774+morphy80@users.noreply.github.com>
2021-05-17 22:37:31 +0200

    Tooltips - Lecture 168

--- section_20_ui_lib_project/src/index.js
+++ section_20_ui_lib_project/src/index.js
@@ -1,3 +1,7 @@
-import './test.css'
+import Tooltip from './ninja-ui/tooltip';
 
-console.log('test');
+// create a tooltip
+
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
